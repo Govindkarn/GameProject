7d1cdfe2289faad62efe97c64c472485a8325475
Govind <govindkarn777@gmail.com>
2024-09-23 23:25:48 +0545

    first commit

--- hello1/src/main/java/com/crud/hello1/UserService.java
+++ hello1/src/main/java/com/crud/hello1/UserService.java
@@ -14,7 +14,7 @@ public class UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-
+ 
     public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
     }
